5942048a260dee90b52f1c549eeace580f05e915
Aryan <aryan.ritesh002@gmail.com>
2022-10-06 13:40:58 +0530

    v1.0.0

--- database/db.js
+++ database/db.js
@@ -6,22 +6,18 @@ dotenv.config();
 const USERNAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD;
 
-const Connection = () => {
-  const MONGODB_URI = `mongodb://aryan307:123@ac-yqzhstr-shard-00-00.uoeirjf.mongodb.net:27017,ac-yqzhstr-shard-00-01.uoeirjf.mongodb.net:27017,ac-yqzhstr-shard-00-02.uoeirjf.mongodb.net:27017/?ssl=true&replicaSet=atlas-scnr9k-shard-0&authSource=admin&retryWrites=true&w=majority`;
+const Connection = async () => {
+  const MONGODB_URI = `mongodb://${USERNAME}:${PASSWORD}@ac-yqzhstr-shard-00-00.uoeirjf.mongodb.net:27017,ac-yqzhstr-shard-00-01.uoeirjf.mongodb.net:27017,ac-yqzhstr-shard-00-02.uoeirjf.mongodb.net:27017/?ssl=true&replicaSet=atlas-scnr9k-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
-  mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-
-  mongoose.connection.on("connected", () => {
-    console.log("Database Connected Successfully!");
-  });
-
-  mongoose.connection.on("disconnected", () => {
-    console.log("Database Disconnected");
-  });
-
-  mongoose.connection.on("error", () => {
-    console.log("Error While Connecting To The Database", error.message);
-  });
+  try {
+    await mongoose.connect(MONGODB_URI, {
+      useUnifiedTopology: true,
+      useNewURLParser: true,
+    });
+    console.log("Connected To Database Successfully");
+  } catch (error) {
+    console.log("Error While Connecting To Database", error);
+  }
 };
 
 export default Connection;
